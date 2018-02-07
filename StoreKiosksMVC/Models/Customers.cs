8bddb79f2f91c7565011421eaccfd77261c88878
tdonkor <tdonkor@gmail.com>
2018-02-07 13:49:09 +0000

    Added new Customer class  and other updates

---
+++ StoreKiosksMVC/Models/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreKiosksMVC.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        [StringLength(100, MinimumLength = 1)]
+        public int CustomerName { get; set; }
+    }
+}
